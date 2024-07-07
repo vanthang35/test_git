ac1634194216e572ace08681c73e97e917a164ec
van thang <nguyenvanthangtnlop95@gmail.com>
2024-07-07 20:04:51 +0700

    simple task

---
+++ luyencode/include/Student.h
@@ -0,0 +1,28 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+
+#include <iostream>
+#include <iomanip>
+#include <Person.h>
+class Student : public Person
+{
+    private:
+    float gpa = 0.0;
+    string currentCourse;
+    const string studentId;
+    static int numStudents;
+    public:
+    Student();
+    Student(const string &, const string &, char, const string &, float, const string &, const string &);
+    Student( const Student&);
+    ~Student();
+    void Print() const;
+    float GetGPA() const {return gpa;}
+    const string &GetCurrentCourse() const {return currentCourse;}
+    const string &GetStudentId() const {return studentId;}
+    void SetCurrentCourse(const string &);
+    static int GetNumberStudents();
+    
+};
+#endif
---
+++ luyencode/src/main.cpp
@@ -0,0 +1,9 @@
+#include <Person.h>
+#include <Student.h>
+
+int main()
+{
+    Student s1("Jo", "Li", 'U', "Ms.", 3.9, "C++", "178PSU");
+    s1.Print();
+    return 0;
+}
